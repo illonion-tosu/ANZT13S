3e3044cab420a3bb1d1480c178fc84236e0f825c
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-01-24 16:48:06 +1100

    Complete rendering of teams

--- get-seed-reveal/index.js
+++ get-seed-reveal/index.js
@@ -3,17 +3,37 @@ let teamStats = []
 async function submit() {
     const textareaElValue = textareaEl.value
     const textAreaElValueSplit = textareaElValue.split("\n")
-    for (let i = 1; i < textAreaElValueSplit.length; i++) {
-        const textAreaElValueSplitSplit = textAreaElValueSplit[i].split(",")
+    for (let i = 0; i < textAreaElValueSplit.length; i++) {
+        const textAreaElValueSplitSplit = textAreaElValueSplit[i].split("\t")
         const teamStat = {
             "team_name": textAreaElValueSplitSplit[0],
-            "aim": Number(textAreaElValueSplitSplit[1]),
-            "speed": Number(textAreaElValueSplitSplit[2]),
-            "stamina": Number(textAreaElValueSplitSplit[3]),
-            "finger_control": Number(textAreaElValueSplitSplit[4]),
-            "precision": Number(textAreaElValueSplitSplit[5]),
-            "reading": Number(textAreaElValueSplitSplit[6]),
-            "technical": Number(textAreaElValueSplitSplit[7])
+            "team_seed": Number(textAreaElValueSplitSplit[1]),
+            "player1-name": textAreaElValueSplitSplit[2],
+            "player1-rank": Number(textAreaElValueSplitSplit[3]),
+            "player2-name": textAreaElValueSplitSplit[4],
+            "player2-rank": Number(textAreaElValueSplitSplit[5]),
+            "aim1-score": Number(textAreaElValueSplitSplit[6]),
+            "aim1-rank": Number(textAreaElValueSplitSplit[7]),
+            "aim2-score": Number(textAreaElValueSplitSplit[8]),
+            "aim2-rank": Number(textAreaElValueSplitSplit[9]),
+            "aim3-score": Number(textAreaElValueSplitSplit[10]),
+            "aim3-rank": Number(textAreaElValueSplitSplit[11]),
+            "tap1-score": Number(textAreaElValueSplitSplit[12]),
+            "tap1-rank": Number(textAreaElValueSplitSplit[13]),
+            "tap2-score": Number(textAreaElValueSplitSplit[14]),
+            "tap2-rank": Number(textAreaElValueSplitSplit[15]),
+            "tap3-score": Number(textAreaElValueSplitSplit[16]),
+            "tap3-rank": Number(textAreaElValueSplitSplit[17]),
+            "ctl1-score": Number(textAreaElValueSplitSplit[18]),
+            "ctl1-rank": Number(textAreaElValueSplitSplit[19]),
+            "ctl2-score": Number(textAreaElValueSplitSplit[20]),
+            "ctl2-rank": Number(textAreaElValueSplitSplit[21]),
+            "ctl3-score": Number(textAreaElValueSplitSplit[22]),
+            "ctl3-rank": Number(textAreaElValueSplitSplit[23]),
+            "gim1-score": Number(textAreaElValueSplitSplit[24]),
+            "gim1-rank": Number(textAreaElValueSplitSplit[25]),
+            "gim2-score": Number(textAreaElValueSplitSplit[26]),
+            "gim2-rank": Number(textAreaElValueSplitSplit[27])
         }
         teamStats.push(teamStat)
     }
@@ -22,6 +42,6 @@ async function submit() {
     const blob = new Blob([jsonString], { type: "application/json" })
     const link = document.createElement("a")
     link.href = URL.createObjectURL(blob)
-    link.download = "team-stats.json"
+    link.download = "teams-seed-reveal.json"
     link.click()
 }
---
+++ seed-reveal/index.js
@@ -0,0 +1,41 @@
+import { loadTeamsSeedReveal } from "../_shared/core/teams.js"
+
+let currentTeamNumber = 0
+
+let allTeamsSeedReveal  = []
+Promise.all([loadTeamsSeedReveal()]).then(([teams]) => {
+    // Load teams
+    allTeamsSeedReveal = teams
+    renderTeam()
+})
+
+// Team Data
+const teamSeedEl = document.getElementById("team-seed")
+const teamNameEl = document.getElementById("team-name")
+const playerInfoElementIds = ["player1-name", "player2-name", "player1-rank", "player2-rank"]
+const mapScoreElementIds = [
+    "aim1-score", "aim2-score", "aim3-score", "aim1-rank", "aim2-rank", "aim3-rank",
+    "tap1-score", "tap2-score", "tap3-score", "tap1-rank", "tap2-rank", "tap3-rank",
+    "ctl1-score", "ctl2-score", "ctl3-score", "ctl1-rank", "ctl2-rank", "ctl3-rank",
+    "gim1-score", "gim2-score", "gim1-rank", "gim2-rank"
+]
+
+// Load current team
+function renderTeam() {
+    const data = allTeamsSeedReveal[currentTeamNumber]
+
+    // Team Info
+    teamSeedEl.textContent = `#${data.team_seed}`
+    teamNameEl.textContent = data.team_name
+
+    // Player Info
+    for (let i = 0; i < playerInfoElementIds.length; i++) {
+        document.getElementById(playerInfoElementIds[i]).textContent = playerInfoElementIds[i].endsWith("rank") ? `#${data[playerInfoElementIds[i]]}` : data[playerInfoElementIds[i]]
+    }
+
+    // Score Info
+    for (let i = 0; i < mapScoreElementIds.length; i++) {
+        const text = mapScoreElementIds[i].endsWith("rank") ? `#${data[mapScoreElementIds[i]]}` : data[mapScoreElementIds[i]].toLocaleString()
+        document.getElementById(mapScoreElementIds[i]).textContent = text
+    }
+}
