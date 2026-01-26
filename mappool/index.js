11249181bedd167e0496ebf804e64f8466c91984
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-01-26 13:59:02 +1100

    Start on star system

--- mappool/index.js
+++ mappool/index.js
@@ -1,4 +1,5 @@
 import { loadBeatmaps, findBeatmap } from "../_shared/core/beatmaps.js"
+import { createTosuWsSocket } from "../_shared/core/websocket.js"
 
 // Load mappool
 const roundNameEl = document.getElementById("round-name")
@@ -8,3 +9,72 @@ Promise.all([loadBeatmaps()]).then(([beatmaps]) => {
     allBeatmaps = beatmaps.beatmaps
     roundNameEl.textContent = `// ${beatmaps.roundName} mappool`
 })
+
+// Updating Team Names
+// Socket
+const socket = createTosuWsSocket()
+socket.onmessage = event => {
+    const data = JSON.parse(event.data)
+    console.log(data)
+}
+
+// Update Star Count Buttons
+const setStarRedPlusEl = document.getElementById("set-star-red-plus")
+const setStarRedMinusEl = document.getElementById("set-star-red-minus")
+const setStarBluePlusEl = document.getElementById("set-star-blue-plus")
+const setStarBlueMinusEl = document.getElementById("set-star-blue-minus")
+
+/* Next Autopick */
+const nextAutopickNextEl = document.getElementById("next-autopick-text")
+const nextAutopickRedEl = document.getElementById("next-autopick-red")
+const nextAutopickBlueEl = document.getElementById("next-autopick-blue")
+const toggleAutopickButtonEl = document.getElementById("toggle-autopick-button")
+const toggleAutopickOnOffEl = document.getElementById("toggle-autopick-on-off")
+let isAutopickOn = false, currentPicker = "red"
+
+// Toggle stars button
+const toggleStarButtonEl = document.getElementById("toggle-stars-button")
+const toggleStarsOnOffEl = document.getElementById("toggle-stars-on-off")
+document.addEventListener("DOMContentLoaded", () => {
+    toggleStarButtonEl.addEventListener("click", () => toggleStars(toggleStarsOnOffEl, toggleStarButtonEl, redTeamStarContainerEl, blueTeamStarContainerEl))
+    document.cookie = `toggleStarContainers=${true}; path=/`
+
+    // Update star count buttons
+    setStarRedPlusEl.addEventListener("click", () => updateStarCount("red", "plus", redTeamStarContainerEl, blueTeamStarContainerEl, redTeamName, blueTeamName))
+    setStarRedMinusEl.addEventListener("click", () => updateStarCount("red", "minus", redTeamStarContainerEl, blueTeamStarContainerEl, redTeamName, blueTeamName))
+    setStarBluePlusEl.addEventListener("click", () => updateStarCount("blue", "plus", redTeamStarContainerEl, blueTeamStarContainerEl, redTeamName, blueTeamName))
+    setStarBlueMinusEl.addEventListener("click", () => updateStarCount("blue", "minus", redTeamStarContainerEl, blueTeamStarContainerEl, redTeamName, blueTeamName))
+
+    // Toggle Autopick button
+    toggleAutopickButtonEl.addEventListener("click", function() {
+        isAutopickOn = !isAutopickOn
+        toggleAutopickOnOffEl.textContent = isAutopickOn ? "ON" : "OFF"
+        toggleAutopickButtonEl.classList.toggle("toggle-on", isAutopickOn)
+        toggleAutopickButtonEl.classList.toggle("toggle-off", !isAutopickOn)
+    })
+
+    // Set Autopicker Buttons
+    nextAutopickRedEl.addEventListener("click", () => setAutopicker("red"))
+    nextAutopickBlueEl.addEventListener("click",() => setAutopicker("blue"))
+
+    // Ban Pick Management
+    banPickManagementSelectActionEl.addEventListener("click", setBanPickAction)
+
+    // Current Picker
+    currentPickerRedEl.addEventListener("click", () => updateCurrentPicker("red"))
+    currentPickerBlueEl.addEventListener("click", () => updateCurrentPicker("blue"))
+    currentPickerNoneEl.addEventListener("click", () => updateCurrentPicker("none"))
+    currentPickerNoneEl.click()
+})
+
+const banPickManagementSelectActionEl = document.getElementById("ban-pick-management-select-action")
+
+// Setting current picker
+const currentPickerTextEl = document.getElementById("current-picker-text")
+const currentPickerRedEl = document.getElementById("current-picker-red")
+const currentPickerBlueEl = document.getElementById("current-picker-blue")
+const currentPickerNoneEl = document.getElementById("current-picker-none")
+function updateCurrentPicker(side) {
+    currentPickerTextEl.textContent = side
+    document.cookie = `currentPicker=${side}; path=/`
+}
